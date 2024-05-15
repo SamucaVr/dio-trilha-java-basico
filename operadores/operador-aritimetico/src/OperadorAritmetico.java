0eba9968efd78d38eef079ba702bd700b4274a17
Samuel Vieira <vieirasamuel000@gmail.com>
2024-05-15 18:08:16 -0300

    feat: adiciona aviso ao ultilzar o operador de adição

--- operadores/operador-aritimetico/src/OperadorAritmetico.java
+++ operadores/operador-aritimetico/src/OperadorAritmetico.java
@@ -7,6 +7,26 @@ public class OperadorAritmetico {
         int multiplicacao = 20 * 7;
         int divisao = 15 / 3;
         int modulo = 18 % 3;
-        double resultado = (10 * 7) + (20/4);
+        double resultado = (10 * 7) + (20 / 4);
+
+        // Alguns cuidados que devem ser tomados quando for usar o operador de Adição.
+
+        // qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
     }
 }
