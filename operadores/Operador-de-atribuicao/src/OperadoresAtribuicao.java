7f22bbbd26c9074e4eea720428e8a5a15d7c176a
Samuel Vieira <vieirasamuel000@gmail.com>
2024-05-15 18:01:29 -0300

    feat: adicionar arquivo operador de atribuição

---
+++ operadores/Operador-de-atribuicao/src/OperadoresAtribuicao.java
@@ -0,0 +1,10 @@
+public class OperadoresAtribuicao {
+    public static void main(String[] args) throws Exception {
+        // Operador de Atribuição
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+    }
+}
