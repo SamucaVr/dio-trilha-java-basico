9d6ff4089f3e156c08d7981173caaf1cfd1dc9e5
Samuel Vieira <vieirasamuel000@gmail.com>
2024-05-15 18:05:41 -0300

    feat: adiciona arquivo dos operadores aritmético

---
+++ operadores/operador-aritimetico/src/OperadorAritmetico.java
@@ -0,0 +1,12 @@
+public class OperadorAritmetico {
+    public static void main(String[] args) throws Exception {
+        // Operadores Aritméticos
+        System.out.println("Hello, World!");
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+    }
+}
