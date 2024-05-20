7556cb9881158cacdec918abeddc576188ed0909
Samuel Vieira <vieirasamuel000@gmail.com>
2024-05-20 19:52:42 -0300

    feat: adiciona todos os arquivos do exercícios

---
+++ java-doc/src/Calculadora.java
@@ -0,0 +1,21 @@
+/**
+ * <h1>Calculadora</h1>
+ * <p> A Calculadora realiza operações matemáticas entre números inteiros </p>
+ * <p> <strong> Note: </strong> Leia atentamente a documentação desta classe para desfrutar dos recursos oferecidos pelo autor </p>
+ * @author Samuel Vieira
+ * @version 1.0
+ * @since 20/05/2024
+ */
+
+public class Calculadora {
+    
+    /**
+     * Este método é utilizado para calcular dois número inteiros
+     * @param numeroUm Este é o primeiro índice do parâmetro
+     * @param numeroDois Este é o segundo índice do parâmetro
+     * @return Retorna Int, pois o método soma dois número inteiros
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
